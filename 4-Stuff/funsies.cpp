0c2e883f8faff629f19499d922861dcf80c116f2
Bob Caddy <r.Caddy@pitt.edu>
2020-04-30 10:50:10 -0400

    add some stuff

--- 4-Stuff/funsies.cpp
+++ 4-Stuff/funsies.cpp
@@ -3,30 +3,38 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::is_floating_point;
 /* 
 test comment color
  */
+
 int main()
 {
-    cout << "Please input a number: ";
-    float Num1;
-    cin >> Num1;
+    cout.precision(20);
+    const double d=0.12345678901234567890;
+    const float  f=0.12345678901234567890;
 
-    cout << "Please input a number: ";
-    float Num2;
-    cin >> Num2;
+    #define PRINT(x) (cout << "hi" << "\n"; cout.flush())
 
-    if (Num1 > Num2)
-    {
-        cout << Num1 << " is greater than " << Num2 << ".";
-    }
-    else if (Num2 > Num1)
-    {
-        cout << Num2 << " is greater than " << Num1 << ".";
-    }
-    else
-    {
-        cout << Num1 << " and " << Num2 << " are the same.";
-    }
+    // cout << "Please input a number: ";
+    // float Num1;
+    // cin >> Num1;
+
+    // cout << "Please input a number: ";
+    // float Num2;
+    // cin >> Num2;
+
+    // if (Num1 > Num2)
+    // {
+    //     cout << Num1 << " is greater than " << Num2 << ".";
+    // }
+    // else if (Num2 > Num1)
+    // {
+    //     cout << Num2 << " is greater than " << Num1 << ".";
+    // }
+    // else
+    // {
+    //     cout << Num1 << " and " << Num2 << " are the same.";
+    // }
 }
 
